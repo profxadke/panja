25fe64c1a38066bb95ff8d09d775823c9558e47c
Prof. - Xadk3! <xadkeg@duck.com>
2024-08-14 12:19:58 +0545

    misc. TUI thingy; TODO: complete web UI ASAP.

--- tui/main.py
+++ tui/main.py
@@ -18,9 +18,11 @@ def main():
 
     while 1:
         try:
-            prompt = input("\x1b[1mYou\x1b[0m: ")
+            print("\x1b[1mYou\x1b[0m:", end=' ', flush=True)
+            prompt = input("")
             while not prompt:
-                prompt = input("\n\x1b[1mYou\x1b[0m: ")
+                print("\x1b[1mYou\x1b[0m:", end=' ', flush=True)
+                prompt = input("")
         except KeyboardInterrupt:
             prompt = 'die'
             print('exit')
