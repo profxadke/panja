1cdefbdc47cd69de35fccb61213af3b57dcecc99
Prof. - Xadk3! <xadkeg@duck.com>
2024-08-14 15:12:23 +0545

    Web UI done! This commit closes #1

--- wui/main.py
+++ wui/main.py
@@ -1,50 +1,45 @@
 #!/usr/bin/env python
 
+from contextlib import asynccontextmanager
 import google.generativeai as gai
 from os import environ as env
 from dotenv import load_dotenv
 from fastapi import FastAPI, HTTPException
-from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 
 
-load_dotenv(); chat = None
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    global Model, Chat
+    Model = gai.GenerativeModel('gemini-1.5-flash')
+    Chat = Model.start_chat()
+    yield
+    del Model, Chat
+
+
+load_dotenv()
 gai.configure(api_key=env["KEY"])
-app = FastAPI(); api = app
+app = FastAPI(lifespan=lifespan); api = app
 
 
 class Prompt(BaseModel):
     message: str
 
 
-@api.get('/')
-def redirect_docs():
-    return RedirectResponse('/docs')
-
-
-@api.get('/chat')
-def init_chat():
-    global chat
-    model = gai.GenerativeModel('gemini-1.5-flash')
-    chat = model.start_chat()
-    return {"resp": "Chat Initialized!"}
-
-
 @api.post('/chat')
 def chat(prompt: Prompt):
     try:
-        # TODO: Better exception handling: ex prompt ~ Are there any public holidays nearby?
-        reply = chat.send_message(prompt.message)
+        reply = Chat.send_message(prompt.message)
         # print(reply)
     except Exception as e:
-        raise HTTPException(status_code=505, detail=f"Server Error: {e}")
-    # TODO: integrate frontend, with markdown rendering.
+        raise HTTPException(status_code=555, detail=f"Server Error: {e}")
     return {"resp": reply.text.strip()}
 
 
-app.mount("/", StaticFiles(directory="root"), name="root")
+# TODO: Enable rendering of markdown (API response)
+app.mount("/", StaticFiles(directory="root", html = True), name="root")
 
 
 if __name__ == '__main__':
-    __import__('uvicorn').run('main:app', host='0.0.0.0', port=2580)
+    __import__('uvicorn').run('main:app', host='0.0.0.0', port=2580, reload=True)
--- wui/root/main.js
+++ wui/root/main.js
@@ -1,4 +1,30 @@
-function sendMessage() {
+const renderMD = markdown => {
+  return DOMPurify.sanitize( marked.parse( markdown ) );
+}
+
+
+async function getMessageResponse(message) {
+  /*
+  fetch('/chat', {
+    method: "POST",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify({message: message})
+  }).then( response => { 
+    response.json().then( json_resp => {
+      return json_resp.resp
+    })
+  })
+  */
+  resp = await fetch('/chat', {
+    method: "POST",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify({message: message})
+  }); resp = await resp.json()
+  return resp.resp
+}
+
+
+async function sendMessage() {
     const input = document.getElementById('chatInput');
     const message = input.value.trim();
 
@@ -7,9 +33,17 @@ function sendMessage() {
         input.value = '';
 
         // FetchAPI for API responses
+        appendMessage('', 'bot');
+        chats = document.querySelectorAll('.chat-bubble');
+        chats[chats.length - 1].remove()
+        const response = await getMessageResponse(message);
+        appendMessage(renderMD(response), 'bot');
+        /*
         setTimeout(() => {
             appendMessage('This is a bot response.', 'bot');
         }, 1e3);
+        */
+
     }
 }
 
@@ -18,7 +52,7 @@ function appendMessage(message, type) {
     const chatMessages = document.getElementById('chatMessages');
     const bubble = document.createElement('div');
     bubble.classList.add('chat-bubble', type);
-    bubble.textContent = message;
+    bubble.innerHTML = message;
     chatMessages.appendChild(bubble);
     chatMessages.scrollTop = chatMessages.scrollHeight;
 }
